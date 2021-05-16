b46653e1ba2622da497496ca50facd708558c660
willian-silva7 <willian.1994@hotmail.com>
2021-05-16 22:17:20 +0100

    Heroku port config

--- src/server.js
+++ src/server.js
@@ -47,6 +47,6 @@ app.use((err, request, response) => {
   });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('  🚀 Server started on port 3333  ');
 });
